2203fff7d38130299564b340b97cf7c2af868c36
teitonny <tonnytei4@gmail.com>
2022-10-18 10:52:11 +0300

    Added the mobile menu using main.js

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-let logo = document.querySelector('#logo-icon')
-    logo.addEventListener('click',function() {
-        alert("this is me")
+let mobileMenu= document.querySelector('.mobile-menu')
+let navList = document.querySelector('.nav_icon-two')
+    mobileMenu.addEventListener('click',()=> {
+        mobileMenu.classList.toggle('active');
+        navList.classList. toggle('active');
     });
+
+    document.querySelectorAll('.nav-link').forEach((nav) => nav.addEventListener('click', () => {
+        mobileMenu.classList.remove('active');
+        navList.classList.remove('active');
+      }));
+      
