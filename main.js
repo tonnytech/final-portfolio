0ec84969b60e476dd8a1e445c80887d960e68b0b
teitonny <tonnytei4@gmail.com>
2022-10-18 12:55:43 +0300

    Added eslint.json file

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-let mobileMenu= document.querySelector('.mobile-menu')
-let navList = document.querySelector('.nav_icon-two')
-    mobileMenu.addEventListener('click',()=> {
-        mobileMenu.classList.toggle('active');
-        navList.classList. toggle('active');
-    });
-
-    document.querySelectorAll('.nav-link').forEach((nav) => nav.addEventListener('click', () => {
-        mobileMenu.classList.remove('active');
-        navList.classList.remove('active');
-      }));
-      
+const mobileMenu = document.querySelector('.mobile-menu');
+const navList = document.querySelector('.nav_icon-two');
+mobileMenu.addEventListener('click', () => {
+  mobileMenu.classList.toggle('active');
+  navList.classList.toggle('active');
+});
+document.querySelectorAll('.nav-link').forEach((nav) => nav.addEventListener('click', () => {
+  mobileMenu.classList.remove('active');
+  navList.classList.remove('active');
+}));
